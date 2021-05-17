ffd36c174b6a1258a8f3e5db15c3327f7ee36c00
João Vinicius Vitral <65374802+JxVtrl@users.noreply.github.com>
2021-05-17 14:28:43 -0300

    Update 1021NotaseMoedas.js

--- URI/exercicios/1021NotaseMoedas.js
+++ URI/exercicios/1021NotaseMoedas.js
@@ -1,4 +1,5 @@
-var num = Number(91.01)
+                                                                                                                                                                 /* @jxvtrl */
+var num = Number(91.01)                                                                              // PARA A ENTRADA DO VALOR PELA PLATAFORMA URI USE: Number(lines.shift())
 var notas = {nota1:0,nota2:0,nota5:0,nota10:0,nota20:0,nota50:0,nota100:0}
 var moedas = {moeda1:0,moeda0_50:0,moeda0_25:0,moeda0_10:0,moeda0_05:0,moeda0_01:0}
 
