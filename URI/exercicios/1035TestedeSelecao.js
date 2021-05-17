e2fff73d590b0499528640fbd6a6a68ffd502cbc
João Vinicius Vitral <joaoviniciusvitral@hotmail.com>
2021-05-17 14:53:55 -0300

    Teste de Seleção 1

--- URI/exercicios/1035TestedeSelecao.js
+++ URI/exercicios/1035TestedeSelecao.js
@@ -1,16 +1,11 @@
 /* @jxvtrl */
-//PARA ENTRADA DO VALOR USE: lines.shift()
 
-var numeros = ('2 3 2 6').split(" ", 4)
+
+var numeros = (lines.shift()).split(" ", 4)
 var A = numeros[0]
-console.log(A)
 var B = numeros[1]
-console.log(B)
 var C = numeros[2]
-console.log(C)
 var D = numeros[3]
-console.log(D)
-
 
 if(B > C && D > A && (C+D) > (A+B) && C > 0 && D > 0 && A%2 == 0){
     console.log("Valores aceitos")
