03789fa9f14a56dfbf7c0cb367b6a6fc6ed84013
João Vinicius Vitral <joaoviniciusvitral@hotmail.com>
2021-05-17 15:54:27 -0300

    Imposto de Renda

--- URI/exercicios/1051ImpostodeRenda.js
+++ URI/exercicios/1051ImpostodeRenda.js
@@ -1,24 +1,19 @@
-var entrada = 1701.12
-entrada = Number(entrada)
+/* @jxvtrl */
+//PARA ENTRADA DO VALOR USE: lines.shift()
 
-switch(entrada){
-    case entrada <= 2000:
-        console.log('Isento')
-        break
-    case entrada <= 3000:
-        var res = parseFloat((entrada-2000)*(8/100))
-        console.log(res.toFixed(2))
-        break
-    case entrada <= 4500:
-        var res = parseFloat(((entrada-2000)*(8/100)) + ((entrada-1000)*(18/100)))
-        console.log(res.toFixed(2))
-        break
-    case entrada > 4500:
-        var res = parseFloat(((entrada-2000)*(8/100)) + ((entrada-1000)*(18/100)) + ((entrada-1500)*(28/100)))
-        console.log(res.toFixed(2))
-        break
+var entrada = Number(lines.shift())
+if((entrada > 2000) && (entrada<=3000)){
+    var res = Number((entrada-2000)*(8/100))
+    console.log(`R$ ${res.toFixed(2)}`)
 }
-console.log(res)
-
-
-
+else if((entrada > 3000) && (entrada <= 4500)){
+    var res = Number(((1000)*(8/100)) + ((entrada-3000)*(18/100)))
+    console.log(`R$ ${res.toFixed(2)}`)
+}
+else if(entrada > 4500){
+    var res = Number(((999.99)*(8/100)) + ((1499.99)*(18/100)) + ((entrada-4500)*(28/100)))
+    console.log(`R$ ${res.toFixed(2)}`)
+}
+else{
+    console.log('Isento')
+}
