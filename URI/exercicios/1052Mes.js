146807b4b89db32895ac35bce306dbd8d6b51cba
João Vinicius Vitral <joaoviniciusvitral@hotmail.com>
2021-05-17 17:22:13 -0300

    Mês

--- URI/exercicios/1052Mes.js
+++ URI/exercicios/1052Mes.js
@@ -1,6 +1,9 @@
 /* @jxvtrl */
 //PARA ENTRADA DO VALOR USE: lines.shift()
 
+var input = require('fs').readFileSync('/dev/stdin', 'utf8');
+var lines = input.split('\n');
+
 var entrada = Number(lines.shift())
 switch(entrada){
     case 1:
