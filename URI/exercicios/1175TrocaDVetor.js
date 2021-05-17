7d4ba9fa785a459ccdbca5930e680b5514cdb780
João Vinicius Vitral <joaoviniciusvitral@hotmail.com>
2021-05-17 18:22:57 -0300

    Troca em Vetor I

--- URI/exercicios/1175TrocaDVetor.js
+++ URI/exercicios/1175TrocaDVetor.js
@@ -1,6 +1,15 @@
+/* @jxvtrl */
+//PARA ENTRADA DO VALOR USE: lines.shift()
+
+var input = require('fs').readFileSync('/dev/stdin', 'utf8');
+var lines = input.split('\n');
+
 var N = []
 
-
+for(var c = 19;c>=0;c--){
+    var entrada = Number(lines.shift())
+    N.push(entrada)
+}
 for(var n = 0;n<=19;n++){
 console.log(`N[${n}] = ${N[19-n]}`)
 }
