26918c01399285f489b4be03b58823a09f919058
João Vinicius Vitral <joaoviniciusvitral@hotmail.com>
2021-05-17 17:23:49 -0300

    Quadrado de Pares

--- URI/exercicios/1073QuadradodePares.js
+++ URI/exercicios/1073QuadradodePares.js
@@ -1,6 +1,9 @@
 /* @jxvtrl */
 //PARA ENTRADA DO VALOR USE: lines.shift()
 
+var input = require('fs').readFileSync('/dev/stdin', 'utf8');
+var lines = input.split('\n');
+
 var entrada = Number(lines.shift())
 for(var c = 1; c<=entrada;c++){
     if(c%2==0){
