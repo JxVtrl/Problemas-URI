bc79f79deb900e51109a29a327b426125eb0d542
João Vinicius Vitral <joaoviniciusvitral@hotmail.com>
2021-05-17 15:08:25 -0300

    Triângulo

--- URI/exercicios/1043Triangulo.js
+++ URI/exercicios/1043Triangulo.js
@@ -1,14 +1,12 @@
 /* @jxvtrl */
 //PARA ENTRADA DO VALOR USE: lines.shift()
 
-var entrada = ('6.0 4.0 2.1').split(" ", 3)
+var entrada = lines.shift().split(" ", 3)
 var A = Number(entrada[0])
 var B = Number(entrada[1])
 var C = Number(entrada[2])
 var perimetro = A+B+C
 var trap = ((A+B)*C)/2
-console.log(trap)
-console.log(perimetro)
 
 if((B - C)< A && A < (B+C) == true ){
     if((A - C)  < B && B < (A + C) == true){
