3686fdce242edb435d648a24300a05ad51c38bc5
João Vinicius Vitral <joaoviniciusvitral@hotmail.com>
2021-05-17 17:22:18 -0300

    Números Pares

--- URI/exercicios/1059Pares.js
+++ URI/exercicios/1059Pares.js
@@ -1,6 +1,9 @@
 /* @jxvtrl */
 //PARA ENTRADA DO VALOR USE: lines.shift()
 
+var input = require('fs').readFileSync('/dev/stdin', 'utf8');
+var lines = input.split('\n');
+
 for(var c = 1;c<=100;c++){
     if(c%2==0){
         console.log(c)
