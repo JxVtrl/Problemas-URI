2b98a40b9205272bab31b7700aa99576bc7f9466
João Vinicius Vitral <joaoviniciusvitral@hotmail.com>
2021-05-17 17:50:44 -0300

    Soma de Pares Consecutivos

--- URI/exercicios/1159SomaParConseq.js
+++ URI/exercicios/1159SomaParConseq.js
@@ -1,24 +1,31 @@
+/* @jxvtrl */
+//PARA ENTRADA DO VALOR USE: lines.shift()
+
+var input = require('fs').readFileSync('/dev/stdin', 'utf8');
+var lines = input.split('\n');
+
 var parar = false
 do{
-var entrada = -1
-if(entrada==0){
-    parar = true
-}
-else if(entrada%2==0){
-    var soma = entrada
-    for(var c = 0; c<4;c++){
-        entrada += 2
-        soma += entrada
+var entrada = Number(lines.shift())
+    if(entrada==0){
+        parar = true
     }
-}
-else if(entrada%2!=0){
-    entrada += 1
-    var soma = entrada
-    for(var c = 0; c<4;c++){
-        entrada += 2
-        soma += entrada
+    else{ 
+        if(entrada%2===0){
+            var soma = entrada
+            for(var c = 0; c<=3;c++){
+                entrada += 2
+                soma += entrada
+            }
+        }
+        else{
+            entrada += 1
+            var soma = entrada
+            for(var c = 0; c<=3;c++){
+                entrada += 2
+                soma += entrada
+            }
+        }
     }
-}
 console.log(soma)
-
-}while(parar==false)
+}while(parar!=true)
