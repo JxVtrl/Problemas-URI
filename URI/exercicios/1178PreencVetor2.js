d8cf4a46815b367280967e06ad0d4e795c1cd6db
João Vinicius Vitral <joaoviniciusvitral@hotmail.com>
2021-05-17 18:33:16 -0300

    Preenchimento de Vetor III

--- URI/exercicios/1178PreencVetor2.js
+++ URI/exercicios/1178PreencVetor2.js
@@ -1,15 +1,18 @@
-var entrada = 200.0000
+/* @jxvtrl */
+//PARA ENTRADA DO VALOR USE: lines.shift()
+
 var  N = []
-for(var c = 1;c<=100;c++){
+var entrada = (200.0000).toFixed(4)
+for(var c = 0;c<100;c++){
     if(entrada == 0.0001){
         N.push(entrada)
-        console.log(`N[${c-1}] = ${(N[c-1])}`)
+        console.log(`N[${c}] = ${(N[c])}`)
         var entrada = (0.0000).toFixed(4)
         N.push(entrada)
     }
     else{
         N.push(entrada)
-        console.log(`N[${c-1}] = ${(N[c-1])}`)
+        console.log(`N[${c}] = ${(N[c])}`)
         var entrada = (entrada/2).toFixed(4)
     }
 }
