dc8fd1f34b8befe2a7759947ef6469d020b889f9
João Vinicius Vitral <joaoviniciusvitral@hotmail.com>
2021-05-17 17:29:26 -0300

    Resto da Divisão

--- URI/exercicios/1133RestodaDiv.js
+++ URI/exercicios/1133RestodaDiv.js
@@ -1,14 +1,19 @@
-var n1 = 10
-var n2 = 18
-var num = []
+/* @jxvtrl */
+//PARA ENTRADA DO VALOR USE: lines.shift()
+
+var input = require('fs').readFileSync('/dev/stdin', 'utf8');
+var lines = input.split('\n');
+
+var n1 = Number(lines.shift())
+var n2 = Number(lines.shift())
+
 if(n2<n1){
     var temp = n1
     n1 = n2
-    n2 = tempS
+    n2 = temp
 }
 for(var c=n1+1;c<n2;c++){
     if(c%5 == 2 || c%5 == 3){
-        console.log(c)
-               
+        console.log(c)           
     }
 }
