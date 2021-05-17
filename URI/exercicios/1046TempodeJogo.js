6cb30b664a6cd672fcd1e81cc335dff101085233
João Vinicius Vitral <joaoviniciusvitral@hotmail.com>
2021-05-17 15:14:44 -0300

    Tempo de Jogo

--- URI/exercicios/1046TempodeJogo.js
+++ URI/exercicios/1046TempodeJogo.js
@@ -1,41 +1,18 @@
-var entrada = ('7 10 8 9').split(" ", 4)
-var h1 = Number(entrada[0])
-var h2 = Number(entrada[2])
-var m1 = Number(entrada[1])
-var m2 = Number(entrada[3])
+/* @jxvtrl */
+//PARA ENTRADA DO VALOR USE: lines.shift()
 
-if(h1 == h2){
-    var horas = 0
-    if(m1==m2){
-        horas = 24
-        var minutos = 0
-    }
+var entrada = lines.shift().split(" ", 2)
+var h1 = Number(entrada[0])
+var h2 = Number(entrada[1])
+
+if(h1 == 0 && h2 == 0){
+    var tempo = 24
 }
 else if(h2 > h1){
-    var horas = h2 - h1
-    if(m1 > m2){
-        var minutos = 60-(m1-m2)
-        horas--
-    }
-    else if(m1 == m2){
-        var minutos = 0
-    }
-    else if(m2>m1){
-        var minutos = m2-m1
-    }
+    var tempo = h2 - h1
 }
-else if (h2 < h1){
-    var horas = (24-h1)+h2
-    if(m1 > m2){
-        var minutos = 60-(m1-m2)
-    }
-    else if(m1 == m2){
-        var minutos = 0
-    }
-    else if(m2>m1){
-        var minutos = m2-m1
-    }
+else{
+    var tempo = (24-h1)+h2
 }
 
-
-console.log(`O JOGO DUROU ${horas} HORA(S) E ${minutos} MINUTO(S)`)
+console.log(`O JOGO DUROU ${tempo} HORA(S)`)
