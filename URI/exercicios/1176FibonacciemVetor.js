94e6d32087ec5c25326db839e78322cc0400f214
João Vinicius Vitral <joaoviniciusvitral@hotmail.com>
2021-05-17 18:24:00 -0300

    Fibonacci em Vetor

--- URI/exercicios/1176FibonacciemVetor.js
+++ URI/exercicios/1176FibonacciemVetor.js
@@ -1,7 +1,12 @@
-var qtd = 1
+/* @jxvtrl */
+//PARA ENTRADA DO VALOR USE: lines.shift()
+
+var input = require('fs').readFileSync('/dev/stdin', 'utf8');
+var lines = input.split('\n');
+
+var qtd = Number(lines.shift())
 for(var c = 0;c<qtd;c++){
-    var pos = 10
-    var soma = 0
+    var pos = Number(lines.shift())
     var fib = [0,1]
     for(var n = 0;n<pos;n++){
         fib.push(fib[n]+fib[(n+1)])
