7205399fc0a3c035396c00cf2e2a49bbed223aec
João Vinicius Vitral <joaoviniciusvitral@hotmail.com>
2021-05-17 18:57:46 -0300

    Coordenadas de um Ponto

---
+++ URI/exercicios/1041CoordDeUmPonto.js
@@ -0,0 +1,31 @@
+/* @jxvtrl */
+//PARA ENTRADA DO VALOR USE: lines.shift()
+
+var input = require('fs').readFileSync('/dev/stdin', 'utf8');
+var lines = input.split('\n');
+
+var cord = lines.shift().split(" ")
+var x = Number(cord[0])
+var y = Number(cord[1])
+
+if(x == 0 && y== 0){
+    console.log("Origem")
+}
+else if(x == 0){
+    console.log("Eixo Y")
+}
+else if(y == 0){
+    console.log("Eixo X")
+}
+else if(x > 0 && y > 0){
+    console.log("Q1")
+}
+else if(x < 0 && y > 0){
+    console.log("Q2")
+}
+else if(x < 0 && y < 0){
+    console.log("Q3")
+}
+else if(x > 0 && y < 0){
+    console.log("Q4")
+}
