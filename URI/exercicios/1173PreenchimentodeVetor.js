d028e504c1cef8212045bd5ccd591d7cc2bf00fa
João Vinicius Vitral <joaoviniciusvitral@hotmail.com>
2021-05-17 18:22:11 -0300

    Preenchimento de Vetor I

--- URI/exercicios/1173PreenchimentodeVetor.js
+++ URI/exercicios/1173PreenchimentodeVetor.js
@@ -1,5 +1,11 @@
+/* @jxvtrl */
+//PARA ENTRADA DO VALOR USE: lines.shift()
+
+var input = require('fs').readFileSync('/dev/stdin', 'utf8');
+var lines = input.split('\n');
+
 var N = []
-var X = -1
+var X = Number(lines.shift())
 
 for(var c = 1;c<=10;c++){
     console.log(`N[${c-1}] = ${X}`)
