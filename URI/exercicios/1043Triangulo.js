ce994e21f552aaac59b74ce3b474f4df3abb9519
João Vinicius Vitral <joaoviniciusvitral@hotmail.com>
2021-05-17 17:21:34 -0300

    Triângulo

--- URI/exercicios/1043Triangulo.js
+++ URI/exercicios/1043Triangulo.js
@@ -1,6 +1,9 @@
 /* @jxvtrl */
 //PARA ENTRADA DO VALOR USE: lines.shift()
 
+var input = require('fs').readFileSync('/dev/stdin', 'utf8');
+var lines = input.split('\n');
+
 var entrada = lines.shift().split(" ", 3)
 var A = Number(entrada[0])
 var B = Number(entrada[1])
