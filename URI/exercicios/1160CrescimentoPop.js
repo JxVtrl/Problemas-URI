ccdbb9fdb74ae1a7a78e9d611b03a284d1ec1399
João Vinicius Vitral <joaoviniciusvitral@hotmail.com>
2021-05-17 18:16:36 -0300

    Crescimento Populacional

--- URI/exercicios/1160CrescimentoPop.js
+++ URI/exercicios/1160CrescimentoPop.js
@@ -1,23 +1,30 @@
-var casos = 1
-do{
-    var entrada = ('100 150 1.0 0').split(" ", 4)
+/* @jxvtrl */
+//PARA ENTRADA DO VALOR USE: lines.shift()
+
+var input = require('fs').readFileSync('/dev/stdin', 'utf8');
+var lines = input.split('\n');
+
+var casos = Number(lines.shift())
+while(casos>0){
+    var entrada = (lines.shift()).split(" ", 4)
     var PA = Number(entrada[0])
     var PB = Number(entrada[1])
     var G1 = Number(entrada[2])
     var G2 = Number(entrada[3])
+    var anos = 0
 
+    for(anos = 0;PA<=PB;anos++){
+    PA += parseInt(PA*(G1/100))
+    PB += parseInt(PB*(G2/100))
+    }
 
-    for(var anos = 0;PA<=PB;anos++){
-    PA += parseInt((PA*(G1/100)))
-    PB += parseInt((PB*(G2/100)))
     if(anos>100){
         console.log("Mais de 1 seculo.")
-        break
-    }
-    }
-    if(anos<=100){
-    console.log(`${anos} anos.`)
-    }
+        casos--
 
-    casos--
-}while(casos>0)
+    }
+    else if(anos<=100){
+        console.log(`${anos} anos.`)
+        casos--
+    }
+}
