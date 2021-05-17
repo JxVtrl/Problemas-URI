18e1e5d0bfb301a21f7dba5a907c0b4843b2fac5
João Vinicius Vitral <joaoviniciusvitral@hotmail.com>
2021-05-17 14:45:50 -0300

    Notas e Moedas

--- URI/exercicios/1021NotaseMoedas.js
+++ URI/exercicios/1021NotaseMoedas.js
@@ -1,5 +1,5 @@
 /* @jxvtrl */
-//PARA ENTRADA DO VALOR USE: Number(lines.shift())
+//PARA ENTRADA DO VALOR USE: lines.shift()
 
 var num = Number(91.01)
 var notas = {nota1:0,nota2:0,nota5:0,nota10:0,nota20:0,nota50:0,nota100:0}
