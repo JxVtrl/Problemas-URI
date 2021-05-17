6cd01841263b6b4bb3ca12b6b9d0ea1df992aba3
João Vinicius Vitral <joaoviniciusvitral@hotmail.com>
2021-05-17 18:36:26 -0300

    Hello World!

---
+++ URI/exercicios/1000HelloWorld.js
@@ -0,0 +1,7 @@
+/* @jxvtrl */
+//PARA ENTRADA DO VALOR USE: lines.shift()
+
+var input = require('fs').readFileSync('/dev/stdin', 'utf8');
+var lines = input.split('\n');
+
+console.log('Hello World!')
