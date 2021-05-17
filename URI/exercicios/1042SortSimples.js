0228a027f295772a6179c87d217baa3db11feb1b
João Vinicius Vitral <joaoviniciusvitral@hotmail.com>
2021-05-17 15:07:31 -0300

    Sort Simples

--- URI/exercicios/1042SortSimples.js
+++ URI/exercicios/1042SortSimples.js
@@ -1,21 +1,17 @@
 /* @jxvtrl */
 //PARA ENTRADA DO VALOR USE: lines.shift()
 
-var entrada = ('7 21 -14').split(" ", 3)
+var entrada = (lines.shift()).split(" ", 3)
 var limpa = []
 limpa[0] = Number(entrada[0])
 limpa[1] = Number(entrada[1])
 limpa[2] = Number(entrada[2])
-
 limpa = limpa.sort(function(a, b){return a - b})
+
 for(var c = 0; c <= 2; c++){
     console.log(limpa[c])
 }
 console.log('')
-
 for(var d = 0; d <= 2; d++){
     console.log(entrada[d])
 }
-
-
-
