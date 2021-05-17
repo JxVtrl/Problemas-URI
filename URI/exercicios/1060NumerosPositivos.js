49d4097bc500bd129a65d442496950c5c5412dc3
João Vinicius Vitral <joaoviniciusvitral@hotmail.com>
2021-05-17 17:22:23 -0300

    Números Positivos

--- URI/exercicios/1060NumerosPositivos.js
+++ URI/exercicios/1060NumerosPositivos.js
@@ -1,6 +1,9 @@
 /* @jxvtrl */
 //PARA ENTRADA DO VALOR USE: lines.shift()
 
+var input = require('fs').readFileSync('/dev/stdin', 'utf8');
+var lines = input.split('\n');
+
 var n = 0
 for(var c = 0;c<=6;c++){
     var num = Number(lines.shift())
