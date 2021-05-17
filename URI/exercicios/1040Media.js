5d5ffbdd2e539f8eacad41a122f3a1cf00d7e08c
João Vinicius Vitral <joaoviniciusvitral@hotmail.com>
2021-05-17 15:06:38 -0300

    Média 3

--- URI/exercicios/1040Media.js
+++ URI/exercicios/1040Media.js
@@ -1,15 +1,15 @@
 /* @jxvtrl */
 //PARA ENTRADA DO VALOR USE: lines.shift()
 
-var notas = ('10.0 10.0 0.0 0.0').split(" ", 4)
-var final = Number('5.0')
+var notas = (lines.shift('')).split(" ", 4)
+var final = Number(lines.shift())
 var N1 = notas[0]
 var N2 = notas[1]
 var N3 = notas[2]
 var N4 = notas[3]
 var media = Number(((N1*2+N2*3+N3*4+N4*1)/10).toFixed(1))
-console.log(`Media: ${media.toFixed(1)}`)
 
+console.log(`Media: ${media.toFixed(1)}`)
 if (media >= 5 && media <= 6.9){
     console.log("Aluno em exame.")
     console.log(`Nota do exame: ${final.toFixed(1)}`)
@@ -22,7 +22,6 @@ if (media >= 5 && media <= 6.9){
         console.log("Aluno reprovado.")
         console.log(`Media final: ${mediaFinal}`)
     }
-    
 }
 else if(media < 5){
     console.log("Aluno reprovado.")
