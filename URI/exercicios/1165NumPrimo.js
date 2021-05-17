bbd676102e86b8e3f0ab88c15706e0a5b3051624
João Vinicius Vitral <joaoviniciusvitral@hotmail.com>
2021-05-17 18:20:41 -0300

    Número Primo

--- URI/exercicios/1165NumPrimo.js
+++ URI/exercicios/1165NumPrimo.js
@@ -1,6 +1,12 @@
-var qtdNum = 1
+/* @jxvtrl */
+//PARA ENTRADA DO VALOR USE: lines.shift()
+
+var input = require('fs').readFileSync('/dev/stdin', 'utf8');
+var lines = input.split('\n');
+
+var qtdNum = Number(lines.shift())
 do{
-    var entrada = 8
+    var entrada = Number(lines.shift())
     var dois = 2
     for(var c=1;c<=entrada;c++){
         if(entrada%c==0){
