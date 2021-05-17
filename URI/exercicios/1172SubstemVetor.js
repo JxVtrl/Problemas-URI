11ff38c176448fa2fb3ac48a8b0b19981aaa2ad3
João Vinicius Vitral <joaoviniciusvitral@hotmail.com>
2021-05-17 18:21:25 -0300

    Substituição em Vetor I

--- URI/exercicios/1172SubstemVetor.js
+++ URI/exercicios/1172SubstemVetor.js
@@ -1,17 +1,14 @@
-var X = [-1,
-    -45234,
-    -22,
-    -120,
-    0,
-    2,
-    -200,
-    1,
-    9,
-    22]
-    for(c=0;c<10;c++){
-        
-        if(X[c]<=0){
-            X[c] = 1
-        }
-        console.log(`X[${c}] = ${X[c]}`)
+/* @jxvtrl */
+//PARA ENTRADA DO VALOR USE: lines.shift()
+
+var input = require('fs').readFileSync('/dev/stdin', 'utf8');
+var lines = input.split('\n');
+
+var X = []
+for(c=0;c<10;c++){
+    X[c] = Number(lines.shift())
+    if(X[c]<=0){
+        X[c] = 1
     }
+    console.log(`X[${c}] = ${X[c]}`)
+}
