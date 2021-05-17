0ac83ff0541e7bf62b35796d5debf59b75ae29da
João Vinicius Vitral <joaoviniciusvitral@hotmail.com>
2021-05-17 18:20:00 -0300

    Número Perfeito

--- URI/exercicios/1164NumPerfeito.js
+++ URI/exercicios/1164NumPerfeito.js
@@ -1,6 +1,12 @@
-var qtdNum = 1
+/* @jxvtrl */
+//PARA ENTRADA DO VALOR USE: lines.shift()
+
+var input = require('fs').readFileSync('/dev/stdin', 'utf8');
+var lines = input.split('\n');
+
+var qtdNum = Number(lines.shift())
 do{
-    var entrada = 6
+    var entrada = Number(lines.shift())
     var soma = 0
     for(var c=1;c<entrada;c++){
         if(entrada%c==0){
@@ -13,5 +19,5 @@ do{
     else{
         console.log(`${entrada} nao eh perfeito`)
     }
-    
+    qtdNum--
 }while(qtdNum>0)
