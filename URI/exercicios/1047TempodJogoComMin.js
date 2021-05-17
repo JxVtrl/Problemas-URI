ba05e70bf28cf6d174c4397eae8d40b3503efdf9
João Vinicius Vitral <joaoviniciusvitral@hotmail.com>
2021-05-17 19:07:22 -0300

    Tempo de Jogo com Minutos

---
+++ URI/exercicios/1047TempodJogoComMin.js
@@ -0,0 +1,50 @@
+
+var entrada = ('1 59 2 58').split(" ", 4)
+var h1 = Number(entrada[0])
+var h2 = Number(entrada[2])
+var m1 = Number(entrada[1])
+var m2 = Number(entrada[3])
+
+if(h1 == h2){
+    var horas = 0
+    if(m1==m2){
+        horas = 24
+        var minutos = 0
+    }
+    else if(m1>m2){
+        horas = 23
+        var minutos = 60-(m1-m2)
+    }
+    else{
+        horas = 23
+        var minutos = 60-(m2-m1)
+    }
+}
+else if(h2 > h1){
+    var horas = h2 - h1
+    if(m1 > m2){
+        var minutos = 60-(m1-m2)
+        horas--
+    }
+    else if(m1 == m2){
+        var minutos = 0
+    }
+    else if(m2>m1){
+        var minutos = m2-m1
+    }
+}
+else if (h2 < h1){
+    var horas = (24-h1)+h2
+    if(m1 > m2){
+        var minutos = 60-(m1-m2)
+        horas--
+    }
+    else if(m1 == m2){
+        var minutos = 0
+    }
+    else if(m2>m1){
+        var minutos = m2-m1
+    }
+}
+
+console.log(`O JOGO DUROU ${horas} HORA(S) E ${minutos} MINUTO(S)`)
