5c4eda4777ac67cdf35e7838af1ec99a19533c89
João Vinicius Vitral <joaoviniciusvitral@hotmail.com>
2021-05-17 17:21:12 -0300

    Lanche 1.0

--- URI/exercicios/1038LanchesForma1.js
+++ URI/exercicios/1038LanchesForma1.js
@@ -1,6 +1,9 @@
 /* @jxvtrl */
 //PARA ENTRADA DO VALOR USE: lines.shift()
 
+var input = require('fs').readFileSync('/dev/stdin', 'utf8');
+var lines = input.split('\n');
+
 var pedido = (lines.shift()).split(" ", 2)
 
 const CQ = 4
