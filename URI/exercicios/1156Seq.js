39de0060d6e11fd6de5f74b3aaa0f3ba7de0b4ae
João Vinicius Vitral <joaoviniciusvitral@hotmail.com>
2021-05-17 17:43:03 -0300

    Sequência S II

--- URI/exercicios/1156Seq.js
+++ URI/exercicios/1156Seq.js
@@ -1,3 +1,9 @@
+/* @jxvtrl */
+//PARA ENTRADA DO VALOR USE: lines.shift()
+
+var input = require('fs').readFileSync('/dev/stdin', 'utf8');
+var lines = input.split('\n');
+
 var den = 1
 var s = 0
 for(var num = 1;num<=39;num+2){
