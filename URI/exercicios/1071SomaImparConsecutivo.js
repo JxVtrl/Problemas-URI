6c8416c9d8c10c981f51c9b70ee0dd772ae6685b
João Vinicius Vitral <joaoviniciusvitral@hotmail.com>
2021-05-17 16:25:46 -0300

    Soma de Impares Consecutivos I

--- URI/exercicios/1071SomaImparConsecutivo.js
+++ URI/exercicios/1071SomaImparConsecutivo.js
@@ -1,6 +1,12 @@
-var entrada = [15,12]
+/* @jxvtrl */
+//PARA ENTRADA DO VALOR USE: lines.shift()
+
+var entrada = []
 var impares = []
 var soma = 0
+for(var c = 0; c<2;c++){
+    entrada[c] = Number(lines.shift())
+}
 if(entrada[0]>entrada[1]){
     var n1 = entrada[1]
     var n2 = entrada[0]
@@ -15,7 +21,6 @@ if(n1==n2){
 else{
 
 for(var control = (n1+1);control<n2;control++){
-    
     if(control%2!=0){
        impares.push(control) 
     }
